f4e5159ef130ceb5c88b7ffd84ef4ce0691b22e0
Emon <emon.mhk69@gmail.com>
2023-04-08 03:42:19 +0600

    error page done

--- src/Component/Error/Error.jsx
+++ src/Component/Error/Error.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import Lottie from "lottie-react";
+import ErrorAnimation from "../../assets/error.json"
 const Error = () => {
     return (
-        <div>
-            <h2>404 Page Not Found!!!</h2>
+        <div className='flex justify-center items-center h-screen'>
+            <Lottie className='w-full h-2/3' animationData={ErrorAnimation} loop={true} />;
         </div>
     );
 };
