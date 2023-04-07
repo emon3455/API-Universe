f680e41d97cae0cd03a0fcc5405ccb44f3b5e38c
Emon <emon.mhk69@gmail.com>
2023-04-08 03:35:41 +0600

    loading spinner done

--- src/Component/Layout/Main.jsx
+++ src/Component/Layout/Main.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Header from '../Header/Header';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
+import LoaderSpinner from '../LoaderSpinner';
 
 const Main = () => {
+
+    const navigation = useNavigation();
+
     return (
         <div>
             <Header></Header>
             <main className='container mx-auto my-4'>
+                {navigation.state === "loading" && <LoaderSpinner></LoaderSpinner>}
                 <Outlet></Outlet>
             </main>
         </div>
---
+++ src/Component/LoaderSpinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const LoaderSpinner = () => {
+
+    return (
+        <div className='flex justify-center items-center h-screen'>
+            <progress className="progress w-72"></progress>
+        </div>
+    );
+};
+
+export default LoaderSpinner;
