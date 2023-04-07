15cbc932ae97cb98d0a9b327f71abf33d85a7774
Emon <emon.mhk69@gmail.com>
2023-04-08 01:56:34 +0600

    home and about page done

--- src/Component/About/About.jsx
+++ src/Component/About/About.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+// import Lottie from "lottie-react";
+// import aboutAnimation from "../../assets/aboutAnime.json";
 
 const About = () => {
     return (
-        <div>
-            <h2>About Page</h2>
+        <div className='flex flex-col justify-center items-center h-screen'>
+            <h2 className='text-4xl font-bold'>This Projects is ALL About Exploring API AND Routing!!!</h2>
+            {/* <Lottie className=" h-2/3" animationData={aboutAnimation} loop={true} />; */}
         </div>
     );
 };
--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Lottie from "lottie-react";
+import homeAnimation from "../../assets/homeAnime.json";
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is home page</h2>
+        <div className='flex flex-col justify-center items-center h-screen'>
+            <h2 className='text-4xl font-semibold'>Select Your API And Explore!!!</h2>
+            <Lottie className=" h-2/3" animationData={homeAnimation} loop={true} />;
         </div>
     );
 };
